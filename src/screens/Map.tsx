7797d0406cb3a82962d791eefd3692fee2cec9d7
Rafael Feitoza <rafaeljr@outlook.com>
2022-06-26 21:18:35 -0300

    feat(screens): add `MapScreen`

---
+++ src/screens/Map.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { KeyboardAvoidingView, Platform, View } from 'react-native';
+import tailwind from 'twrnc';
+
+import MapComponent from '../components/Map';
+import Ride from '../components/Ride';
+
+const Map = () => {
+  return (
+    <KeyboardAvoidingView
+      style={tailwind`flex-1`}
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      // keyboardVerticalOffset={Platform.OS === 'ios' ? 100 : 0}
+    >
+      <View style={tailwind`h-full`}>
+        <View style={tailwind`h-1/2`}>
+          <MapComponent />
+        </View>
+        <View style={tailwind`h-1/2`}>
+          <Ride />
+        </View>
+      </View>
+    </KeyboardAvoidingView>
+  );
+};
+
+export default Map;
