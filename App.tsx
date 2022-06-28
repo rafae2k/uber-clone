853ec9223423dd21c1ddd9ac5287e1748c70c3e9
Rafael Feitoza <rafaeljr@outlook.com>
2022-06-28 17:23:46 -0300

    feat(app): add location provider

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Provider } from 'react-redux';
+import LocationProvider from './src/context/Location';
 
 import { store } from './src/redux/store';
 import Routes from './src/routes';
@@ -10,7 +11,9 @@ export default function App() {
     <Provider store={store}>
       <NavigationContainer>
         <SafeAreaProvider>
-          <Routes />
+          <LocationProvider>
+            <Routes />
+          </LocationProvider>
         </SafeAreaProvider>
       </NavigationContainer>
     </Provider>
